c9673ab6576579b19553825b74a1c84c2afbc292
= <=>
2013-10-27 00:36:05 +0800

    Push again

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,4 +2,26 @@ class CategoryController < ApplicationController
   def index
     all_category = Category.all
   end
+ 
+  def create
+  end
+  
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+    all_category = Category.all
+    cat = Category.find_by_id(params[:id])
+    articles = 
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
