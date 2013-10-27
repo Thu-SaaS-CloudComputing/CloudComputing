d2818ebf8c9c349515171573c4a257074e8aa69f
cgcgbcbc <cgcgbcbc@163.com>
2013-10-27 21:16:39 +0800

    restore category_controller

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,27 @@
 class CategoryController < ApplicationController
   def index
+    @categories = Category.all
+  end
+ 
+  def create
+  end
+  
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+    @categories= Category.all
+    id = params[:id]
+    id |= 0
+    @articles = Category.find_by_id(id).articles
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
