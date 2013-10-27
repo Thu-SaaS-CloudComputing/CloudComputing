181c200ea2b704dd68c2fa8acb84ac9cd22a3486
cgcgbcbc <cgcgbcbc@163.com>
2013-10-27 21:03:53 +0800

    regenerate

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,28 +1,4 @@
 class CategoryController < ApplicationController
   def index
-    @categories = Category.all
   end
- 
-  def create
-  end
-  
-  def new
-  end
-
-  def edit
-  end
-
-  def show
-    @categories= Category.all
-    id = params[:id]
-    id |= 0
-    @articles = Category.find_by_id(id).articles
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
 end
