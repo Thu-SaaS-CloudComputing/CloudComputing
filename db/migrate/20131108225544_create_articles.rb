1a9e28d766cfd267fe773da301a123e995af14ca
JimmyZJX <604590822@qq.com>
2013-11-08 23:04:56 +0800

    add condition when dropping the tables in migrate...

---
+++ db/migrate/20131108225544_create_articles.rb
@@ -0,0 +1,16 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    if ActiveRecord::Base.connection.table_exists? 'articles'
+      drop_table :articles
+    end
+    create_table :articles do |t|
+      t.string :title
+      t.string :author
+      t.references :category
+      t.string :content, :default => "(No Content)"
+
+      t.timestamps
+    end
+    add_index :articles, :category_id
+  end
+end
