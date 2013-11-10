0d1bef0c7ccd5fbdbbaf0fabd04916916a37f5ef
cgcgbcbc <cgcgbcbc@163.com>
2013-11-11 01:24:54 +0800

    fix config

--- config/application.rb
+++ config/application.rb
@@ -59,8 +59,8 @@ module CloudComputing
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
-    config.assets.precompile = %w( *.js *.css )
-    config.assets.compile = true
+    config.assets.precompile += %w( *.js *.css )
+    config.assets.compile = false
+    config.assets.compress = true
   end
 end
