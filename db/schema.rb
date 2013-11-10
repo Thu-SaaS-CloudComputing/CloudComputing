4ef3545b074be1c79ab46d56f3116adbf929128d
cgcgbcbc <cgcgbcbc@163.com>
2013-11-11 02:12:32 +0800

    run db:migrate on migration AddTagsFieldToArticles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131109184237) do
+ActiveRecord::Schema.define(:version => 20131110181008) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20131109184237) do
     t.string   "content",     :default => "(No Content)"
     t.datetime "created_at",                              :null => false
     t.datetime "updated_at",                              :null => false
+    t.text     "tags"
   end
 
   add_index "articles", ["category_id"], :name => "index_articles_on_category_id"
