3ecb270bd41cb4b71186980381d9e22b6a1c4cb1
= <andyjvan@hotmail.com>
2013-11-11 19:33:13 +0800

    Fix error on category.index page and comments unnecessary rspec test

--- spec/views/category/index.html.haml_spec.rb
+++ spec/views/category/index.html.haml_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe "category/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#describe "category/index.html.haml" do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
