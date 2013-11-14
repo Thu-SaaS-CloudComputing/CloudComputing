ec1cddae011140438c5755e926d40043a86491d2
JimmyZJX <604590822@qq.com>
2013-11-15 00:00:15 +0800

    create table without dropping

--- db/migrate/20131108225544_create_articles.rb
+++ db/migrate/20131108225544_create_articles.rb
@@ -1,8 +1,5 @@
 class CreateArticles < ActiveRecord::Migration
   def change
-    if ActiveRecord::Base.connection.table_exists? 'articles'
-      drop_table :articles
-    end
     create_table :articles do |t|
       t.string :title
       t.string :author
