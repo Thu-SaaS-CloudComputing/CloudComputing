f2359d1a914095ddaec7d510b299c8af3b0b7f0e
JimmyZJX <604590822@qq.com>
2013-11-15 00:06:48 +0800

    creating without dropping

--- db/migrate/20131109184237_create_categories.rb
+++ db/migrate/20131109184237_create_categories.rb
@@ -1,8 +1,5 @@
 class CreateCategories < ActiveRecord::Migration
   def change
-    if ActiveRecord::Base.connection.table_exists? 'categories'
-      drop_table :categories
-    end
     create_table :categories do |t|
       t.string :name
       t.text :description
