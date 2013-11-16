95649b51a5dd2c0faec6e8ddea497e872cdaa722
JoeJ11 <andyjvan@hotmail.com>
2013-11-16 10:00:05 +0800

    Some label effect

--- app/helpers/visitor_index_helper.rb
+++ app/helpers/visitor_index_helper.rb
@@ -5,8 +5,7 @@ module VisitorIndexHelper
       row_data = table_data[row]
       if row_data.has_key?(col)
         row_data[col].each do |lesson|
-          str << "#{lesson.name}"
-          str << "At #{lesson.classroom}"
+          str << ["#{lesson.name}", "At #{lesson.classroom}"]
         end
       end
     end
