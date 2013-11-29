729ad54ef353aa32df5420cd764a2c573631037e
KevinXuxuxu <Kevin.xu.fangzhou@gmail.com>
2013-11-29 17:55:55 +0800

    part of cucumber in authentification

--- features/step_definitions/user_management.rb
+++ features/step_definitions/user_management.rb
@@ -23,8 +23,8 @@ end
 
 Then /^I should see names and studentIDs of the following users: (.*)$/ do |lst|
   if lst.class == String
-    art_list = lst.split(/[, "]+/)
-    art_list.each do |user|
+    user_list = lst.split(/[, "]+/)
+    user_list.each do |user|
       id = User.find_by_name(user).id
       if page.respond_to? :should
         page.should have_content(user)
@@ -42,4 +42,31 @@ end
 
 When /^I follow detail of (.*)$/ do |user|
   page.find_by_id(user+"_detail").click
+end
+
+When /^I follow "(.*)" in switch of priviledge (.*)/ do |sign, priviledge|
+  page.find_by_id(priviledge+"_switch").find_link(sign).click
+end
+
+And /^I should see all priviledges displayed$/ do
+  Priviledge.all.each do |priviledge|
+    if page.respond_to? :should
+        page.should have_content(priviledge.name)
+      else
+        assert page.has_content?(priviledge.name)
+      end
+  end
+end
+
+And /^I should see "(.*)" displayed in the following priviledges: (.*)$/ do |sign, lst|
+  if lst.class == String
+    lst.split(/[, "]+/).each do |priviledge|
+      block = page.find_by_id(priviledge+"_switch")
+      if block.respond_to? :should
+        block.should have_content(sign)
+      else
+        assert block.has_content?(sign)
+      end
+    end
+  end
 end
